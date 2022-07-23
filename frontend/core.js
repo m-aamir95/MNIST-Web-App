cdf0b481eb827fbba59735da30761b6c36fdde97
Aamir <djzaamir@gmail.com>
2022-07-23 14:54:55 +0500

    Working on adding the drawing functionality

--- frontend/core.js
+++ frontend/core.js
@@ -1,4 +1,8 @@
 let root = document.getElementById("root");
+let reset_btn = document.getElementById("reset_btn");
+let pen = document.getElementById("pen");
+
+let isDrawing = false;
 
 //Create the canvas
 let canvas = document.createElement("canvas");
@@ -12,7 +16,28 @@ root.appendChild(canvas);
 let ctx = canvas.getContext("2d");
 
 
+//Add background color
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+reset_btn.addEventListener("click", () => {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+});
+
+
+pen.addEventListener("click", () => {
+    pen.style = "background-color: green; color:white";
+    isDrawing = true;
+});
+
+
+
+canvas.addEventListener("click", () => {
+    console.log("Kclick");
+})
+
+canvas.addEventListener("mousedown", () => {
+    console.log("clicking");
+});
 
