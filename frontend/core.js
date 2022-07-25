3784b7fa32455203357f5e08b27ac20755d0f3a2
Aamir <djzaamir@gmail.com>
2022-07-25 21:06:53 +0500

    Worked on, reset button, pen button and eraser button

--- frontend/core.js
+++ frontend/core.js
@@ -1,8 +1,11 @@
 let root = document.getElementById("root");
 let reset_btn = document.getElementById("reset_btn");
-let pen = document.getElementById("pen");
+let pen_btn = document.getElementById("pen");
+let eraser_btn = document.getElementById("eraser");
 
-let isDrawing = false;
+let isMousePressed = false;
+let isEraser = false;
+let isPen = false;
 
 //Create the canvas
 let canvas = document.createElement("canvas");
@@ -23,35 +26,69 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 reset_btn.addEventListener("click", () => {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height); 
+
+    isPen = false;
+    pen_btn.style = "background-color: brown; color:white";
 });
 
 
-pen.addEventListener("click", () => {
-    pen.style = "background-color: green; color:white";
+pen_btn.addEventListener("click", () => {
+
+    if (!isPen) {
+        pen_btn.style = "background-color: green; color:white";
+        isPen = true;
+    } else {
+        pen_btn.style = "background-color: brown; color:white";
+        isPen = false;
+    }
+    
+});
+
+eraser_btn.addEventListener('click', () => {
+    isEraser = !isEraser;
+    isPen = false;
+
+    pen_btn.style = "background-color: brown; color:white";
 });
 
 
 
 //Is fired when click is lifted
 canvas.addEventListener("click", () => {
-    isDrawing = false;
-})
+    isMousePressed = false;
+});
 
 //Is fired when the click is pressed
 canvas.addEventListener("mousedown", () => {
-    isDrawing = true;
+    isMousePressed = true;
 });
 
 //Is fired when mouse moves
 canvas.addEventListener("mousemove", (evt) => {
     
-    if (isDrawing) {
-        const x = evt.clientX;
-        const y = evt.clientY;
+    if (isMousePressed && isPen) {
+
+        const canvasBoundingBox = canvas.getBoundingClientRect();
+
+        //TODO; exact reason of why this works is not clear
+        const x = evt.clientX - canvasBoundingBox.left;
+        const y = evt.clientY - canvasBoundingBox.top;
         
         ctx.strokeStyle = "white";
         ctx.lineWidth = 6;
         ctx.strokeRect(x, y, 1, 1);
+    } else if (isMousePressed && isEraser) {
+        
+        const canvasBoundingBox = canvas.getBoundingClientRect();
+
+        //TODO; exact reason of why this works is not clear
+        const x = evt.clientX - canvasBoundingBox.left;
+        const y = evt.clientY - canvasBoundingBox.top;
+        
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 6;
+        ctx.strokeRect(x, y, 3, 3);
     }
 
 })
+
